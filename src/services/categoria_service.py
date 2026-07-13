c09c3c3e75237e2e1bd8aa473b0dd72c6c58617b
Fabriziomn <fabriziomn3@gmail.com>
2026-07-13 12:28:53 -0500

    cambio de consulta y verificacion para evitar duplicados en categorias

--- src/services/categoria_service.py
+++ src/services/categoria_service.py
@@ -1,12 +1,19 @@
 from src.config.database import supabase
 from src.schemas.categoria_schema import CategoriaCreate
-from fastapi import HTTPException
+from fastapi import HTTPException,status
 
 class CategoriaService:
 
     @staticmethod
     def crear_categoria(categoria: CategoriaCreate):
+        categoria_existente = supabase.table("categoria").select("id_categoria").ilike("nombre", categoria.nombre.strip()).execute()
+        if categoria_existente.data:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, 
+                detail=f"La categoria '{categoria.nombre}' ya esta registrado en el sistema"
+            )
         data = categoria.model_dump()
+        data["nombre"] = data["nombre"].strip()
         data["activo"] = True
         response = supabase.table("categoria").insert(data).execute()
         
